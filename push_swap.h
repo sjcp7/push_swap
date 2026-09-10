0aeaf9418e247bc0aad812fd63bc682df2b378f2
ljanuari <leosnanej@gmail.com>
2026-09-10 23:32:18 +0100

    enums was add

--- push_swap.h
+++ push_swap.h
@@ -6,25 +6,75 @@
 /*   By: samupedr <samupedr@student.42luanda.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/07 15:03:44 by samupedr          #+#    #+#             */
-/*   Updated: 2026/09/07 15:08:27 by samupedr         ###   ########.fr       */
+/*   Updated: 2026/09/10 16:18:46 by ljanuari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+# include "libft/libft.h"
+# include <stdlib.h>
+
+# include <stdlib.h>
 
 typedef struct s_stack
 {
 	int	*nums;
-	int	len;
+	int	head;
+	int	size;
 	int	capacity;
 }	t_stack;
 
+typedef struct s_bench
+{
+	int	disorder;
+	int	visible;
+	int	strategy;
+	char	*ops;
+	int	total_ops;
+	int	sa;
+	int	sb;
+	int	ss;
+	int	pa;
+	int	pb;
+	int	ra;
+	int	rb;
+	int	rr;
+	int	rra;
+	int	rrb;
+	int	rrr;
+} t_bench;
+
 typedef struct s_state
 {
-	t_stack	a;
-	t_stack	b;
-	char	**ops;
-}	t_state;
+        t_stack *a;
+        t_stack *b;
+	t_bench	bch;
+}       t_state;
 
+enum ops
+{
+	SA,
+	SB,
+	SS,
+	PA,
+	PB,
+	RA,
+	RB,
+	RR,
+	RRA,
+	RRB,
+	RRR
+};
+
+int		ps_abort(t_state *data);
+int		parse(t_state *data, int ac, char *av[]);
+void	swap(t_stack *pilha);
+void	rotate(t_stack *p);
+void	re_rotate(t_stack *p);
+void	push(t_stack *dst, t_stack *src);
+void	selection_sort(t_state *data);
+void	bucket_sort(t_state *data);
+void	radix_sort(t_state *data);
+void	operation(t_state *data, int ops);
 #endif
