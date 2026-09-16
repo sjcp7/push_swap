4eee51f17e0f6c7e18460146156d58b954880e52
Leosnane Januario <ljanuari@c1r8s10.42luanda.com>
2026-09-16 12:50:03 +0100

    update push_swap.h

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -31,19 +31,20 @@ typedef struct s_bench
 	int		visible;
 	int		strategy;
 	char	*ops;
+	int		capacity;
 	int		total_ops;
 }	t_bench;
 
 typedef struct s_state
 {
-	t_stack *a;
-	t_stack *b;
+    	t_stack *a;
+    	t_stack *b;
 	t_bench	bch;
 }	t_state;
 
 enum ops
 {
-	SA,
+	SA = 1,
 	SB,
 	SS,
 	PA,
@@ -56,24 +57,26 @@ enum ops
 	RRR
 };
 
-int		ps_abort(t_state *data); //free data end print 'Error' at stderr
-int		ps_next(t_stack *p); // return the next position of stack
-int		ps_prev(t_stack *p); // return the previous position of stack
+int		ps_abort(t_state *data);
+int		ps_next(t_stack *p);
+int		ps_prev(t_stack *p);
 int		parse(t_state *data, int ac, char *av[]);
-int		compute_strategy(t_state *data); // calc strategy duo to disorder if it isn't setted in parse
+int		compute_strategy(t_state *data);
 void    ps_realloc(t_stack *p);
-// operations
+int	normalizer(t_stack *a);
+void	ps_add_buffer(t_bench *bench, int op);
+void	ps_print_op(t_bench *bench);
+void	ps_merge_sort(int *nums, int l, int r);
 void	swap(t_stack *pilha);
-void	rotate(t_stack *p); 
+void	rotate(t_stack *p);
 void	re_rotate(t_stack *p);
 void	push(t_stack *dst, t_stack *src);
 // my implementations
 void	bubble_sort(t_state *data);
 void	selection_sort(t_state *data);
-void	insertion_sort(t_state *data); // in building...
+void	insertion_sort(t_state *data);
 void	bucket_sort(t_state *data);
 void	radix_sort(t_state *data);
 //
-void	operation(t_state *data, int ops); //This is an intermediate function for using operation functions. You can add the same enum order.
-//                                           In the future, it will save the operations in a buffer to be printed after processing.
+void	operation(t_state *data, int ops);
 #endif
