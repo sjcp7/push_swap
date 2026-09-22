c80aada91bdd27c681547e2c55eb497a19686c7a
Samuel Pedro <redsamuel.cruz7@gmail.com>
2026-09-22 22:46:05 +0100

    skip sorting when the input is already sorted

--- push_swap.c
+++ push_swap.c
@@ -12,6 +12,20 @@
 
 #include "push_swap.h"
 
+static int	is_sorted(t_stack *a)
+{
+	int	i;
+
+	i = 1;
+	while (i < a->size)
+	{
+		if (a->nums[i - 1] > a->nums[i])
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 int	main(int ac, char **av)
 {
 	t_state	data;
@@ -19,12 +33,15 @@ int	main(int ac, char **av)
 	if (ac < 2 || !parse(&data, ac, av))
 		return (0);
 	data.bench.strategy = compute_strategy(&data);
-	if (data.bench.strategy == 1)
-		selection_sort(&data);
-	else if (data.bench.strategy == 2)
-		chunk_sort(&data);
-	else if (data.bench.strategy == 3)
-		radix_sort(&data);
+	if (!is_sorted(data.a))
+	{
+		if (data.bench.strategy == 1)
+			selection_sort(&data);
+		else if (data.bench.strategy == 2)
+			chunk_sort(&data);
+		else if (data.bench.strategy == 3)
+			radix_sort(&data);
+	}
 	if (data.bench.visible)
 		ps_print_bench(data.bench);
 	free(data.a->nums);
