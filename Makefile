0474921503bcbdfea4aaf8d65c9bd4737c29b885
ljanuari <leosnanej@gmail.com>
2026-09-23 10:59:11 +0100

    buffer_manager.c

--- Makefile
+++ Makefile
@@ -18,7 +18,7 @@ LIBFT = $(LIBFT_DIR)/libft.a
 NAME = push_swap
 SRCS = push_swap.c push_swap_utils.c push_swap_utils_two.c push_swap_utils_three.c \
 		ps_atoi.c ps_strcmp.c parse.c print_bench.c operations.c merge_sort.c \
-		radix_sort.c selection_sort.c chunk_sort.c
+		radix_sort.c selection_sort.c chunk_sort.c buffer_manager.c buffer_manager_utils.c
 OBJS = $(SRCS:.c=.o)
 HEADERS = push_swap.h $(LIBFT_DIR)/libft.h
 
--- push_swap.h
+++ push_swap.h
@@ -43,13 +43,6 @@ typedef struct s_bench
 	int		rrr;
 }	t_bench;
 
-typedef struct s_state
-{
-	t_stack	*a;
-	t_stack	*b;
-	t_bench	bench;
-}	t_state;
-
 typedef enum s_operation
 {
 	NONE = 0,
@@ -73,31 +66,44 @@ typedef struct s_ops_buff
 	int			capacity;
 }	t_ops_buff;
 
-int		ps_abort(t_state *data);
-int		ps_next(t_stack *p);
-int		ps_prev(t_stack *p);
-int		ps_strcmp(const char *s1, const char *s2);
-int		ps_atoi(const char *num, long *atoi);
-int		parse(t_state *data, int ac, char **av);
-int		ps_sqrt(int num);
-int		compute_strategy(t_state *data);
-int		normalizer(t_stack *a);
-int		buffer_push(t_ops_buff *buffer, t_operation op);
-void	buffer_flush(t_ops_buff *buffer);
-void	reverse_op(int *index, int size, t_operation *op);
-int		ps_realloc(t_stack *p);
-void	ps_print_bench(t_bench bench);
-int		ps_merge_sort(int *nums, int l, int r);
-void	find_posix(t_state *data, int num);
-void	find_max(t_state *data);
-void	swap(t_stack *pilha);
-void	rotate(t_stack *p);
-void	re_rotate(t_stack *p);
-void	push(t_stack *dst, t_stack *src);
+typedef struct s_state
+{
+	t_stack		*a;
+	t_stack		*b;
+	t_ops_buff	buffer;
+	t_bench		bench;
+}	t_state;
+
+int			ps_abort(t_state *data);
+int			ps_next(t_stack *p);
+int			ps_prev(t_stack *p);
+int			ps_strcmp(const char *s1, const char *s2);
+int			ps_atoi(const char *num, long *atoi);
+int			parse(t_state *data, int ac, char **av);
+int			ps_sqrt(int num);
+int			find_posix(t_state *data, int num);
+int			find_max(t_state *data);
+int			compute_strategy(t_state *data);
+int			normalizer(t_stack *a);
+void		reverse_op(int *index, int size, t_operation *op);
+int			ps_realloc(t_stack *p);
+void		ps_print_bench(t_bench bench);
+int			ps_merge_sort(int *nums, int l, int r);
+void		swap(t_stack *pilha);
+void		rotate(t_stack *p);
+void		re_rotate(t_stack *p);
+void		push(t_stack *dst, t_stack *src);
 // my implementations
 void	selection_sort(t_state *data);
 void	chunk_sort(t_state *data);
 void	radix_sort(t_state *data);
 //
-void	operation(t_state *data, t_operation ops);
+int			operation(t_state *data, t_operation ops);
+// BUFFER_MANAGER FUNCTIONS
+int			buffer_push(t_ops_buff *buffer, t_operation op);
+void		buffer_flush(t_state *data);
+char		*get_op_name(t_operation op);
+int			is_canceled(t_operation a, t_operation b);
+int			is_merged(t_operation a, t_operation b, t_operation *out);
+int			buffer_simplify(t_ops_buff *buffer);
 #endif
